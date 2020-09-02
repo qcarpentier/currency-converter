b841a3593db2c6de2aa508b9f8171567379bd63a
Quentin Carpentier <quentin.carpentier@outlook.be>
2020-09-02 08:50:07 +0200

    ✨ Get Countries using Async/Await

--- currency-converter.js
+++ currency-converter.js
@@ -6,12 +6,19 @@ const axios = require("axios");
 
 const getExchangeRate = async (fromCurrency, toCurrency) => {
   const response = await axios.get("http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1");
-  
+
   const rate = response.data.rates;
   const euro = 1 / rate[fromCurrency];
   const exchangeRate = euro * rate[toCurrency];
 
-  console.log(exchangeRate);
+  return exchangeRate;
 };
 
-getExchangeRate("USD", "EUR");
+const getCountries = async (toCurrency) => {
+  const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${toCurrency}`);
+
+  return response.data.map((country) => country.name);
+};
+
+// getExchangeRate("USD", "EUR");
+getCountries("USD");
