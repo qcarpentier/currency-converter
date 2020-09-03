af80d8d670540940fcfe58ca35e1ebbb0570e280
Quentin Carpentier <quentin.carpentier@outlook.be>
2020-09-03 12:56:32 +0200

    ♻️ Decent refactoring

--- currency-converter.js
+++ currency-converter.js
@@ -1,42 +1,45 @@
 const axios = require("axios");
 
+const FIXER_API_KEY = "f68b13604ac8e570a00f7d8fe7f25e1b";
+const FIXER_API = `http://data.fixer.io/api/latest?access_key=${FIXER_API_KEY}`;
+const REST_COUNTRIES_API = "https://restcountries.eu/rest/v2/currency";
+
 const getExchangeRate = async (fromCurrency, toCurrency) => {
-  const response = await axios.get("http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1");
+  try {
+    const {
+      data: { rates },
+    } = await axios.get(FIXER_API);
 
-  const rate = response.data.rates;
-  const euro = 1 / rate[fromCurrency];
-  const exchangeRate = euro * rate[toCurrency];
+    const euro = 1 / rates[fromCurrency];
+    const exchangeRate = euro * rates[toCurrency];
 
-  if (isNaN(exchangeRate)) {
+    return exchangeRate;
+  } catch (error) {
     throw new Error(`Unable to get currency ${fromCurrency} and ${toCurrency}`);
   }
-
-  return exchangeRate;
 };
 
-const getCountries = async (toCurrency) => {
+const getCountries = async (currencyCode) => {
   try {
-    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${toCurrency}`);
+    const { data } = await axios.get(`${REST_COUNTRIES_API}/${currencyCode}`);
 
-    return response.data.map((country) => country.name);
+    return data.map(({ name }) => name);
   } catch (error) {
     throw new Error(`Unable to get countries that use ${toCurrency}`);
   }
 };
 
 const convertCurrency = async (fromCurrency, toCurrency, amount) => {
-  const countries = await getCountries(toCurrency);
-  const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+  fromCurrency = fromCurrency.toUpperCase();
+  toCurrency = toCurrency.toUpperCase();
 
-  const convertedAmount = amount * exchangeRate;
+  const [exchangeRate, countries] = await Promise.all([getExchangeRate(fromCurrency, toCurrency), getCountries(toCurrency)]);
 
-  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spent these in the following countries: ${countries.map((country) => `\n${country}`)}`;
+  const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+  return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries.map((country) => `\n${country}`)}`;
 };
 
-convertCurrency("USD", "CAD", 20)
-  .then((message) => {
-    console.log(message);
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+convertCurrency("EUR", "CAD", 20)
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error.message));
